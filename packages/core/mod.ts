fbb092395287354c1398fd41301bd50e12c025c4
Andrés Pérez Manríquez <apandresipm@gmail.com>
2022-11-15 13:02:52 -0300

    sixth commit

--- packages/core/mod.ts
+++ packages/core/mod.ts
@@ -1,5 +1,5 @@
 import sortBy from "https://deno.land/x/lodash@4.17.15-es/sortBy.js";
-import { IPrez } from "https://deno.land/x/prezdeno_1@v1.0.1/utils/mod.ts";
+import { IPrez } from "https://deno.land/x/prezdeno_1@v1.0.2/utils/mod.ts";
 
 const prezFn: IPrez = {
     name: "prez",
